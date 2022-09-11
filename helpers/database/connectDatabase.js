839c7c15a5967af5ecfdae96d45ee81d8c1b944b
Emirhan <emirhansesigur@gmail.com>
2022-09-11 15:38:06 +0300

    features of the user were created as a model

--- helpers/database/connectDatabase.js
+++ helpers/database/connectDatabase.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 function connectDatabase(){
     mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
-        console.log("Mongodb Connection Successful..................................");
+        console.log("Mongodb Connection Successful
     })
     .catch(err=>{
         console.error(err); 
---
+++ models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name : {
+        type: String,
+        required: [true, "Please provide a name"],
+    }
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: [true, "Please try different email"]
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please provide a valid name"
+        ]
+    },
+    role: {
+        type: String,
+        default: "user", // admin şeklinde  belirtmediginde user olarek rol belirlenir
+        enum : ["user", "admin"], // 2 farkli deger alabilir, bu ikisi sadece
+    },
+    password: {
+        type: String,
+        minlength: [6, "enter a password with min lenght 6"],
+        required: [true, "Please provide a password"],
+        select: false //degerleri cekmek istedigimiz zaman sifre alınamayacak
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now // yaratildigi tam zamani alabilmek icin default olarak o an
+    },
+    title: {
+        type: String
+    },
+    about: {
+        type: String
+    },
+    place: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    profile_image: {
+        type: String,
+        default: "default.jpg" //kullanıcı foto eklemek zorunda degil
+    },
+    blocked: { // kullanicinin blokladigi biri var mı
+        type: Boolean,
+        default: false
+    }
+});
+
+// Save user to mongodb database
+module.export = 
